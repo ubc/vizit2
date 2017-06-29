1ac11b00dd4852a793cc45434539e1398548832b
Matthew Emery <m.emery@alumni.ubc.ca>
2017-06-29 11:09:03 -0700

    Added to function to avoid constantly overwriting Google BigQuery

--- r-package/exec/populate_course.sh
+++ r-package/exec/populate_course.sh
@@ -14,22 +14,30 @@ if [ ! -d "../results/$SHORT" ]; then
     mkdir ../results/${SHORT}
 fi
 
-python3 ./rbq.py demographic_multiple_choice -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py open_assessment -c ${SHORT} -l 1000000 --auto
+Populate () {
 
-python3 ./rbq.py generalized_video_heat -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py generalized_video_axis -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py generalized_demographics -c ${SHORT} -l 1000000 --auto
+    if [ ! -d "../data/$SHORT/$1" ]; then
+        python3 ./rbq.py $1 -c ${SHORT} -l 1000000 --auto
+    fi
 
-python3 ./rbq.py forum_posts -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py forum_searches -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py forum_views -c ${SHORT} -l 1000000 --auto
+}
 
-python3 ./rbq.py tower_item -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py tower_engage_dirt -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py course_axis -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py external_link_dirt -c ${SHORT} -l 1000000 --auto
-python3 ./rbq.py page_dirt -c ${SHORT} -l 1000000 --auto
+Populate demographic_multiple_choice
+Populate open_assessment
+
+Populate generalized_video_heat
+Populate generalized_video_axis
+Populate generalized_demographics
+
+Populate forum_posts
+Populate forum_searches
+Populate forum_views
+
+Populate tower_item
+Populate tower_engage_dirt
+Populate course_axis
+Populate external_link_dirt
+Populate page_dirt
 
 bash ./download_gcp_material.sh ${SHORT} "${GCLOUD}"
 
@@ -42,5 +50,3 @@ Rscript ./wrangle_video.R ${SHORT}
 Rscript ./wrangle_link_page.R ${SHORT}
 Rscript ./wrangle_general.R ${SHORT}
 Rscript ./wrangle_assessments.R ${SHORT}
-
-
