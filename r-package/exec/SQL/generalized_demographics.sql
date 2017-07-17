0df19657526bde864764765f282123a8a4944f70
Matthew Emery <m.emery@alumni.ubc.ca>
2017-07-17 14:01:13 -0700

    generalized_demographics.sql finished (date filtering, removed username)

--- r-package/exec/SQL/generalized_demographics.sql
+++ r-package/exec/SQL/generalized_demographics.sql
@@ -1,7 +1,35 @@
-SELECT  user_id, username, registered, viewed, explored, certified, completed,
-        cc_by_ip, countryLabel, continent, city, region, un_economic_group, un_developing_nation,
-        LoE, YoB, gender, grade, passing_grade, start_time, nvideos_unique_viewed, nvideos_total_watched,
-        ndays_act, language, mode, sum_dt
-FROM [ubcxdata:{course}.person_course]
-WHERE cc_by_ip IS NOT NULL
-LIMIT {limit}
+SELECT
+  user_id,
+  registered,
+  viewed,
+  explored,
+  certified,
+  completed,
+  cc_by_ip,
+  countryLabel,
+  continent,
+  city,
+  region,
+  un_economic_group,
+  un_developing_nation,
+  LoE,
+  YoB,
+  gender,
+  grade,
+  passing_grade,
+  start_time,
+  nvideos_unique_viewed,
+  nvideos_total_watched,
+  ndays_act,
+  language,
+  mode,
+  sum_dt
+FROM
+  [ubcxdata:{course}.person_course]
+WHERE
+  cc_by_ip IS NOT NULL
+  AND start_time > PARSE_UTC_USEC("{date} 00:00:00")
+ORDER BY
+  start_time
+LIMIT
+  {limit}
