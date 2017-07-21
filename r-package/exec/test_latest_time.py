9161469726da3a94baf1e42fc51fa4549a48e96a
Matthew Emery <m.emery@alumni.ubc.ca>
2017-07-21 15:25:29 -0700

    Added a fixture to test_latest_time

--- r-package/exec/test_latest_time.py
+++ r-package/exec/test_latest_time.py
@@ -2,35 +2,19 @@ from .latest_time import *
 from itertools import chain
 
 
-def test_update_timestamp_json():
-    test_timestamp = {
-        "courses": [{
-            "course": "c_a",
-            "jobs": [{
-                "job": "j_a",
-                "time": "2017-07-03 18:05:38"
-            }]
-        }]
-    }
+import pytest
 
-    new_timestamps = update_timestamp_json(test_timestamp, "c_b", "j_b", "2018-01-01 00:00:00")
+def test_update_timestamp_json(timestamp):
+    new_timestamps = update_timestamp_json(timestamp, "c_b", "j_b", "2018-01-01 00:00:00")
+
     assert "c_b" in [key["course"] for key in new_timestamps['courses']]
     assert "j_b" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
                                               for key in new_timestamps['courses']]))
 
 
-def test_update_timestamp_json_same_course():
-    test_timestamp = {
-        "courses": [{
-            "course": "c_a",
-            "jobs": [{
-                "job": "j_a",
-                "time": "2017-07-03 18:05:38"
-            }]
-        }]
-    }
+def test_update_timestamp_json_same_course(timestamp):
+    new_timestamps = update_timestamp_json(timestamp, "c_a", "j_b", "2018-01-01 00:00:00")
 
-    new_timestamps = update_timestamp_json(test_timestamp, "c_a", "j_b", "2018-01-01 00:00:00")
     assert "c_a" in [key["course"] for key in new_timestamps['courses']]
     assert len([key["course"] for key in new_timestamps['courses']]) == 1
     assert "j_b" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
@@ -38,26 +22,32 @@ def test_update_timestamp_json_same_course():
     assert len(list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
                                          for key in new_timestamps['courses']]))) == 2
 
-    def test_update_timestamp_json_same_course_same_job():
-        test_timestamp = {
-            "courses": [{
-                "course": "c_a",
-                "jobs": [{
-                    "job": "j_a",
-                    "time": "2017-07-03 18:05:38"
-                }]
+def test_update_timestamp_json_same_course_same_job(timestamp):
+    new_timestamps = update_timestamp_json(timestamp, "c_a", "j_a", "2018-01-01 00:00:00")
+    assert "c_a" in [key["course"] for key in new_timestamps['courses']]
+    assert len([key["course"] for key in new_timestamps['courses']]) == 1
+
+    assert "j_a" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
+                                              for key in new_timestamps['courses']]))
+    assert len(list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
+                                         for key in new_timestamps['courses']]))) == 1
+
+    assert "2018-01-01 00:00:00" in list(chain.from_iterable([
+        [inner_key["time"] for inner_key in key["jobs"]]
+        for key in new_timestamps['courses']]))
+
+
+def test_most_recent_job(timestamp):
+    pass
+
+@pytest.fixture()
+def timestamp():
+    return {
+        "courses": [{
+            "course": "c_a",
+            "jobs": [{
+                "job": "j_a",
+                "time": "2017-07-03 18:05:38"
             }]
-        }
-
-        new_timestamps = update_timestamp_json(test_timestamp, "c_a", "j_a", "2018-01-01 00:00:00")
-        assert "c_a" in [key["course"] for key in new_timestamps['courses']]
-        assert len([key["course"] for key in new_timestamps['courses']]) == 1
-
-        assert "j_a" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
-                                                  for key in new_timestamps['courses']]))
-        assert len(list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
-                                             for key in new_timestamps['courses']]))) == 1
-
-        assert "2018-01-01 00:00:00" in list(chain.from_iterable([
-            [inner_key["time"] for inner_key in key["jobs"]]
-            for key in new_timestamps['courses']]))
+        }]
+    }
