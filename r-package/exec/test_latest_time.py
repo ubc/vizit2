694f9f2200b3957093bb4006443f1c50595f4abc
Matthew Emery <m.emery@alumni.ubc.ca>
2017-07-21 14:46:33 -0700

    latest_time_same_course_same_job test passes

--- r-package/exec/test_latest_time.py
+++ r-package/exec/test_latest_time.py
@@ -36,4 +36,28 @@ def test_update_timestamp_json_same_course():
     assert "j_b" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
                                               for key in new_timestamps['courses']]))
     assert len(list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
-                                              for key in new_timestamps['courses']]))) == 2
+                                         for key in new_timestamps['courses']]))) == 2
+
+    def test_update_timestamp_json_same_course_same_job():
+        test_timestamp = {
+            "courses": [{
+                "course": "c_a",
+                "jobs": [{
+                    "job": "j_a",
+                    "time": "2017-07-03 18:05:38"
+                }]
+            }]
+        }
+
+        new_timestamps = update_timestamp_json(test_timestamp, "c_a", "j_a", "2018-01-01 00:00:00")
+        assert "c_a" in [key["course"] for key in new_timestamps['courses']]
+        assert len([key["course"] for key in new_timestamps['courses']]) == 1
+
+        assert "j_a" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
+                                                  for key in new_timestamps['courses']]))
+        assert len(list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
+                                             for key in new_timestamps['courses']]))) == 1
+
+        assert "2018-01-01 00:00:00" in list(chain.from_iterable([
+            [inner_key["time"] for inner_key in key["jobs"]]
+            for key in new_timestamps['courses']]))
