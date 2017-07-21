a6e9e9b153512add6e8fec82fdfaf54333bf91f7
Matthew Emery <m.emery@alumni.ubc.ca>
2017-07-21 14:33:50 -0700

    latest_time_same_course test fails

--- r-package/exec/test_latest_time.py
+++ r-package/exec/test_latest_time.py
@@ -1,4 +1,5 @@
 from .latest_time import *
+from itertools import chain
 
 
 def test_update_timestamp_json():
@@ -16,3 +17,23 @@ def test_update_timestamp_json():
     assert "c_b" in [key["course"] for key in new_timestamps['courses']]
     assert "j_b" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
                                               for key in new_timestamps['courses']]))
+
+
+def test_update_timestamp_json_same_course():
+    test_timestamp = {
+        "courses": [{
+            "course": "c_a",
+            "jobs": [{
+                "job": "j_a",
+                "time": "2017-07-03 18:05:38"
+            }]
+        }]
+    }
+
+    new_timestamps = update_timestamp_json(test_timestamp, "c_a", "j_b", "2018-01-01 00:00:00")
+    assert "c_a" in [key["course"] for key in new_timestamps['courses']]
+    assert len([key["course"] for key in new_timestamps['courses']]) == 1
+    assert "j_b" in list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
+                                              for key in new_timestamps['courses']]))
+    assert len(list(chain.from_iterable([[inner_key["job"] for inner_key in key["jobs"]]
+                                              for key in new_timestamps['courses']]))) == 2
