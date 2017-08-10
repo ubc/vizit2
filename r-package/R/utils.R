20a37eccf2018415518301c466d464d0d1f5e10c
Matthew Emery <m.emery@alumni.ubc.ca>
2017-08-10 13:11:01 -0700

    Added utils and test-utils and other missing files

---
+++ r-package/R/utils.R
@@ -0,0 +1,21 @@
+#' Read CSV course data
+#'
+#' Will add the ".csv" if not included in `csv_name`
+#'
+#' @param course_name string short name of the course
+#' @param csv_name string of the name of the csv
+#' @param data_dir character vector of the relative path
+#'
+#' @return dataframe of read_csv results
+read_course_csv <- function(course_name, csv_name, data_dir = c("..", "data")) {
+  if (endsWith(".csv")) {
+    csv_name <- paste0(csv_name, ".csv")
+  }
+
+  data_dir_string <- paste0(data_dir, collapse = "/")
+
+  file_path <-
+    paste(data_dir_string, input_course, csv_name, sep = "/")
+
+  readr::read_csv(file_path)
+}
---
+++ r-package/tests/testthat/test-utils.R
@@ -0,0 +1,6 @@
+context("Testing Utility Functions")
+
+test_that("Read course CSV can open a CSV", {
+  test_dataframe <- tibble(column1: c("row1", "row2"),
+                           column2: c("row1", "row2"))
+})
