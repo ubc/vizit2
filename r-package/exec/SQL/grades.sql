686cb78f8014c22cc90f14a5b3fd3f3936013b9d
David Laing <davidkendalllaing@gmail.com>
2017-10-03 15:31:27 -0700

    added user-defined function to grades.sql, but haven't made it inline yet

--- r-package/exec/SQL/grades.sql
+++ r-package/exec/SQL/grades.sql
@@ -28,3 +28,26 @@ ON
   A.user_id = B.user_id
 LIMIT
  {limit}
+
+ function getActivityLevel(row,emit) {
+   emit({user_id: row.user_id,
+         gender: row.gender,
+         mode: row.mode,
+         activity_level: decodeHelper(row.sum_dt)})
+ }
+
+ function decodeHelper(x) {
+   if (x < 1800) {return 'under_30_min'}
+   if (x >= 1800 & x < 18000) {return '30_min_to_5_hr'}
+   if (x > 18000) {return 'over_5_hr'}
+ }
+
+ bigquery.defineFunction(
+   'getActivityLevel',                           // Name of the function exported to SQL
+   ['user_id', 'gender', 'mode', 'sum_dt'],                    // Names of input columns
+   [{'name': 'activity_level', 'type': 'string'},
+    {'name': 'user_id', 'type': 'integer'},
+    {'name': 'gender', 'type': 'string'},
+    {'name': 'mode', 'type': 'string'}],  // Output schema
+   getActivityLevel                       // Reference to JavaScript UDF
+ );
