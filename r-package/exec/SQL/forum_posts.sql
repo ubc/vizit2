704a6c5f503ffb8c56b070d7d9739472d0f1d1a2
Scott McMillan <scottmcmillan@gmail.com>
2017-10-06 15:10:04 -0700

    revert forum_posts.sql to original it works...

--- r-package/exec/SQL/forum_posts.sql
+++ r-package/exec/SQL/forum_posts.sql
@@ -1,20 +1,19 @@
-SELECT
-  author_id,
-  author_username,
-  gender,
-  mode AS registration_status,
-  sum_dt,
-  mongoid,
+SELECTT
+	author_id,
+	author_username,
+	gender,
+	mode AS registration_status,
+	sum_dt,
+	mongoid,
   _type AS bq_post_type,
   thread_type AS initial_post_type,
   commentable_id,
   comment_thread_id,
   parent_id,
   title,
-  body,
-  created_at
-FROM (
-  SELECT
+  body
+FROM
+  (SELECT
     author_id,
     author_username,
     mongoid,
@@ -25,24 +24,9 @@ FROM (
     parent_id,
     title,
     body,
-    created_at
-  FROM
-    [ubcxdata:{course}.forum]
-  WHERE
-    created_at > PARSE_UTC_USEC("{date} 00:00:00")) AS A
-INNER JOIN (
-  SELECT
-    user_id,
-    gender,
-    mode,
-    sum_dt
-  FROM
-    [ubcxdata:{course}.person_course]
-  WHERE
-    sum_dt IS NOT NULL) AS B
-ON
-  A.author_id = B.user_id
-ORDER BY
-  created_at
-LIMIT
-  {limit}
+    created_at 
+  FROM [ubcxdata:{course}.forum])
+  as A
+LEFT JOIN [ubcxdata:{course}.person_course] AS B
+ON  A.author_id = B.user_id
+LIMIT {limit}
