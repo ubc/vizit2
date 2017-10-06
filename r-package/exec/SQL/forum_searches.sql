6f51aa6f1a3a09d45a737f082bc7ee1227b327d9
Scott McMillan <scottmcmillan@gmail.com>
2017-10-06 14:47:08 -0700

    reverted form_searches query to original which works

--- r-package/exec/SQL/forum_searches.sql
+++ r-package/exec/SQL/forum_searches.sql
@@ -1,31 +1,16 @@
-SELECT
+SELECT 
   FORMAT_UTC_USEC(time) AS date,
   search_query,
   A.username AS username,
   gender,
   mode AS registration_status,
   sum_dt
-FROM (
-  SELECT
-    *
-  FROM
-    [ubcxdata:{course}.forum_events]
-  WHERE
-    search_query IS NOT NULL
-    AND time > PARSE_UTC_USEC("{date} 00:00:00")) AS A
-INNER JOIN (
-  SELECT
-    username,
-    gender,
-    mode,
-    sum_dt
-  FROM
-    [ubcxdata:{course}.person_course]
-  WHERE
-    sum_dt IS NOT NULL) AS B
-ON
-  A.username = B.username
-ORDER BY
-  date DESC
-LIMIT
-  {limit}
+FROM
+  (SELECT *
+	FROM
+	    [ubcxdata:{course}.forum_events]
+	 WHERE search_query IS NOT NULL) AS A
+LEFT JOIN [ubcxdata:{course}.person_course] AS B
+ON A.username = B.username
+ORDER BY date DESC
+LIMIT {limit}
