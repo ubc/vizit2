f68153244344aa74fb00911661f600791537cb4d
David Laing <davidkendalllaing@gmail.com>
2017-10-18 10:32:17 -0700

    changed general to demographics

---
+++ r-package/exec/wrangle_demographics.R
@@ -0,0 +1,17 @@
+#! /usr/bin/env Rscript 
+# wrangle_demographics.R
+# Andrew Lim, May 2017
+#
+# This script reads in a course's demographic csv and performs necessary wrangling
+# Example: Rscript wrangle_demographics.R marketing
+
+# Load functions and libraries:
+devtools::load_all()
+
+# Read in command line arguments.
+args <- commandArgs(TRUE)
+input_course <- args[1]
+
+# Calling main function
+wrangle_demographics(input_course)
+
