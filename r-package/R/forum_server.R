1917d8671232540c654b8dbd529eb03330f07597
David Laing <davidkendalllaing@gmail.com>
2017-10-24 15:08:15 -0700

    display_name -> commentable_id in count_posts

--- r-package/R/forum_server.R
+++ r-package/R/forum_server.R
@@ -119,7 +119,7 @@ get_target_word_counts <- function(input_forum) {
 count_posts <- function(input_forum, wrangled_forum_elements) {
   # Make sure there is a row for every permutation of display names and post types.
   types_dummy_set <- data_frame(
-    "commentable_id" = rep(wrangled_forum_elements$display_name, each = 4),
+    "commentable_id" = rep(wrangled_forum_elements$commentable_id, each = 4),
     "type" = rep(
       c("Discussion", "Question", "Response", "Comment"),
       times = dim(wrangled_forum_elements)[1]
