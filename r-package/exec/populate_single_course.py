f7b77322fca82192650ee6335454d923457b2af1
David Laing <davidkendalllaing@gmail.com>
2017-12-01 13:58:06 -0800

    Create populate_single_course.py

---
+++ r-package/exec/populate_single_course.py
@@ -0,0 +1,29 @@
+#!/opt/conda/envs/mooc/bin/python
+
+import os
+import subprocess
+import json
+import click as cl
+
+
+@cl.command()
+@cl.option("--overwrite", default=False)
+def populate(overwrite):	
+    command = "bash ./populate_course.sh {short_name} {big_table} {gcloud}".format(
+                short_name="Climate101x__3T2015",
+                big_table="UBCx__Climate101x__3T2015",
+                gcloud="UBCx__Climate101x__3T2015")
+    if overwrite:
+        command = "{} --overwrite=true >> srv/shiny-server/logs/cron.log 2>&1".format(command)
+    print(command)
+    subprocess.call(command, shell=True)
+    write_hashed_courses()
+
+
+def write_hashed_courses():
+    subprocess.call('Rscript write_hashed_courses.R', shell=True)
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    populate()
