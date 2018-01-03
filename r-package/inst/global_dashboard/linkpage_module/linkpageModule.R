ed966b98d77f7a860a0c60fe4ab46e5624fb65a7
David Laing <davidkendalllaing@gmail.com>
2018-01-03 14:55:53 -0800

    clean up linkpage module server

--- r-package/inst/global_dashboard/linkpage_module/linkpageModule.R
+++ r-package/inst/global_dashboard/linkpage_module/linkpageModule.R
@@ -59,27 +59,21 @@ linkpageModule <- function(input, output, session) {
 
 
   # Create module name vector for module filtering
+  output$chap_name_linkpage <- renderUI({
+    
+    ns <- NS("linkpageID")
+    
+    selectInput(ns("module"),
+                "Module:",
+                choices = append("All",
+                                 get_module_vector(item_df = page_name())[-1]),
+                selected = "All")
+    
+  })
 
-
-
-   output$chap_name_linkpage <- renderUI({
-
-                        ns <- NS("linkpageID")
-
-                        selectInput(ns("module"),
-                                    "Module:",
-                                    choices = append("All",get_module_vector(item_df = page_name())[-1]),
-                                    selected = "All")
-    })
-
-
-
- #set all threshold
- student_num_threshold <- 10
- min_click_threshold <- 5
-
-
-
+  # set all threshold
+  student_num_threshold <- 10
+  min_click_threshold <- 5
 
   # Obtain user input for filters and selections:
   gender <- reactive({input$gender})
@@ -89,52 +83,46 @@ linkpageModule <- function(input, output, session) {
 
   # Add reset filtering function
   observeEvent(input$reset_filters, autoDestroy = FALSE, {
+    
+    # Update the activity level.
+    activity_level <- reactive({
+      "All"
+    })
+    
+    # Update the gender.
+    gender <- reactive({
+      "All"
+    })
+    
+    # Update the registration status.
+    mode <- reactive({
+      "All"
+    })
+    
+    module <- reactive({
+      "All"
+    })
+    
+    updateSelectInput(session,
+                      inputId = "activity_level",
+                      selected = activity_level()
+    )
+    
+    updateSelectInput(session,
+                      inputId = "gender",
+                      selected = gender()
+    )
+    
+    updateSelectInput(session,
+                      inputId = "mode",
+                      selected = mode()
+    )
+    updateSelectInput(session,
+                      inputId = "module",
+                      selected = module()
+    )
 
-                        # Update the activity level.
-                        activity_level <- reactive({
-                                "All"
-                        })
-
-                        # Update the gender.
-                        gender <- reactive({
-                                "All"
-                        })
-
-                        # Update the registration status.
-                        mode <- reactive({
-                                "All"
-                        })
-
-                        module <- reactive({
-                                "All"
-                        })
-
-
-
-                        updateSelectInput(session,
-                                          inputId = "activity_level",
-                                          selected = activity_level()
-                        )
-
-                        updateSelectInput(session,
-                                          inputId = "gender",
-                                          selected = gender()
-                        )
-
-                        updateSelectInput(session,
-                                          inputId = "mode",
-                                          selected = mode()
-                        )
-                        updateSelectInput(session,
-                                          inputId = "module",
-                                          selected = module()
-                        )
-
-})
-
-
-
-
+  })
 
 
   #############################################################
@@ -144,64 +132,57 @@ linkpageModule <- function(input, output, session) {
   # Filter student demograpgic
   log_dat_filtered <- reactive({
 
-    log_dat <- filter_demographics(log_dat(), gender = input$gender, mode = input$mode,activity_level = input$activity_level)
+    log_dat <- filter_demographics(log_dat(),
+                                   gender = input$gender,
+                                   mode = input$mode,
+                                   activity_level = input$activity_level)
 
     return(log_dat)
+    
   })
-
-
-
 
   reactive_page <- reactive({
+    
+    # Compute the pageview of each page
+    page_student <- get_pageview(filtered_log_df = log_dat_filtered())
+    
+    # If there are no pages existent after summarization and filtering
+    if (nrow(page_student) == 0) {
+      
+      all_pages <- data.frame('Warning' = 'No page found.')
+      
+    } else {
+      
+      # If there are pages still existent
+      
+      # Join two tables for getting the name of each page, 
+      # common column is path (two hash strings)
+      page_name_mapping <- inner_join(page_student,
+                                      page_name(),
+                                      by = "path")
+      
+      # Filter page by chapter
+      page_name_mapping <- filter_chapter_linkpage(page_name_mapping,
+                                                   module = input$module)
+      
+      # Get page name
+      each_page <- get_page_name(page_name_df = page_name_mapping)
+      
+      # Remove duplicated name for each page
+      all_pages <- get_unique_page_name(each_page_df = each_page)
+      
+    }
 
-  # Compute the pageview of each page
-
-  page_student <- get_pageview(filtered_log_df = log_dat_filtered())
-
-  # If there are no pages existent after summarization and filtering
-
-  if (nrow(page_student)==0){
-
-    all_pages <- data.frame('Warning'= 'No page found.')
-
-
-  # If there are pages still existent
-
-  }else{
-
-
-  # Join two tables for getting the name of each page, common column is path (two hash strings)
-  page_name_mapping <- inner_join(page_student,page_name(),by = "path")
-
-
-  ## Filter page by chapter
-  page_name_mapping <- filter_chapter_linkpage(page_name_mapping,module = input$module)
-
-
-  # Get page name
-  each_page <- get_page_name(page_name_df = page_name_mapping)
-
-
-  # Remove duplicated name for each page
-  all_pages <- get_unique_page_name(each_page_df = each_page)
-
-
-}
-
-
-  return(all_pages)
-
-
+    return(all_pages)
 
   })
-
 
   # Render pages table
   output$page_summary <- DT::renderDataTable({
 
     page_df <- creat_page_table(page_summary = reactive_page())
 
-    DT::datatable(page_df,escape=FALSE)
+    DT::datatable(page_df,escape = FALSE)
 
   })
 
@@ -211,37 +192,48 @@ linkpageModule <- function(input, output, session) {
 
   # Filter demograhics
   reactive_link_dat <- reactive({
-
-
-  link_dat <- filter_demographics(link_dat(), gender = input$gender, mode = input$mode,activity_level = input$activity_level)
-
-  ## Filter chapter
-  link_dat <- filter_chapter_linkpage(link_dat,module = input$module)
-
-  # compute the total click of each external link
-  get_click_per_link(link_df = link_dat)
+    
+    link_dat <- filter_demographics(link_dat(), 
+                                    gender = input$gender, 
+                                    mode = input$mode,
+                                    activity_level = input$activity_level)
+    
+    ## Filter chapter
+    link_dat <- filter_chapter_linkpage(link_dat,
+                                        module = input$module)
+    
+    # Compute the total click of each external link
+    get_click_per_link(link_df = link_dat)
 
   })
-
 
   # Render link summary table
   output$link_summary <- DT::renderDataTable({
 
     link_df <- creat_link_table(link_summary = reactive_link_dat())
 
-    DT::datatable(link_df,escape=FALSE)
+    DT::datatable(link_df,escape = FALSE)
 
-})
+  })
 
   #########################################################
   ################ median statistic  ######################
   #########################################################
 
   output$click_median <- renderText({
-        paste0("Click Median: "," ",median(reactive_link_dat()$number_of_click,na.rm = TRUE))
-        })
+    
+    paste0("Click Median: ",
+           " ",
+           median(reactive_link_dat()$number_of_click, na.rm = TRUE))
+  
+  })
 
   output$pageview_median <- renderText({
-        paste0("Pageview Median: "," ",median(reactive_page()$Pageview,na.rm = TRUE))
-        })
+    
+    paste0("Pageview Median: ",
+           " ",
+           median(reactive_page()$Pageview, na.rm = TRUE))
+        
+  })
+  
 }
