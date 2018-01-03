280df546f8ae96a311a4be28b0dcdb0d8c6eb338
David Laing <davidkendalllaing@gmail.com>
2018-01-03 14:01:00 -0800

    reformatted

--- r-package/inst/global_dashboard/overview_module/overviewModule.R
+++ r-package/inst/global_dashboard/overview_module/overviewModule.R
@@ -28,24 +28,23 @@ overviewModule <- function(input, output, session) {
                 paste0(root, requested_course(), "/tower_engage.csv"),
                 read_csv)
 
-   # Create module name vector for module filtering
+  # Create module name vector for module filtering
 
   reactive_get_module_vector <- reactive({
     validate(need(try(nrow(tower_item()) > 0), "No modules found."))
     get_module_vector(item_df = tower_item())[-1]
   })
 
-   output$chap_name_overview <- renderUI({
-
-                        ns <- NS("overviewID")
-
-                        selectInput(ns("module"),
-                                    "Module:",
-                                    choices = append("All", reactive_get_module_vector()),
-                                    selected = "All")
-    })
-
-
+  output$chap_name_overview <- renderUI({
+    
+    ns <- NS("overviewID")
+    
+    selectInput(ns("module"),
+                "Module:",
+                choices = append("All", reactive_get_module_vector()),
+                selected = "All")
+    
+  })
 
   # Obtain user input for filters and selections:
   gender <- reactive({input$gender})
@@ -53,53 +52,50 @@ overviewModule <- function(input, output, session) {
   mode <- reactive({input$mode})
   module <- reactive({input$module})
 
-
-
   # Add reset filtering button
   observeEvent(input$reset_filters, autoDestroy = FALSE, {
+    
+    # Update the activity level.
+    activity_level <- reactive({
+      "All"
+    })
+    
+    # Update the gender.
+    gender <- reactive({
+      "All"
+    })
+    
+    # Update the registration status.
+    mode <- reactive({
+      "All"
+    })
+    
+    # Update the category.
+    module <- reactive({
+      "All"
+    })
+    
+    updateSelectInput(session,
+                      inputId = "activity_level",
+                      selected = activity_level()
+    )
+    
+    updateSelectInput(session,
+                      inputId = "gender",
+                      selected = gender()
+    )
+    
+    updateSelectInput(session,
+                      inputId = "mode",
+                      selected = mode()
+    )
+    
+    updateSelectInput(session,
+                      inputId = "module",
+                      selected = module()
+    )
 
-                        # Update the activity level.
-                        activity_level <- reactive({
-                                "All"
-                        })
-
-                        # Update the gender.
-                        gender <- reactive({
-                                "All"
-                        })
-
-                        # Update the registration status.
-                        mode <- reactive({
-                                "All"
-                        })
-
-                        # Update the category.
-                        module <- reactive({
-                                "All"
-                        })
-
-                        updateSelectInput(session,
-                                          inputId = "activity_level",
-                                          selected = activity_level()
-                        )
-
-                        updateSelectInput(session,
-                                          inputId = "gender",
-                                          selected = gender()
-                        )
-
-                        updateSelectInput(session,
-                                          inputId = "mode",
-                                          selected = mode()
-                        )
-
-                        updateSelectInput(session,
-                                          inputId = "module",
-                                          selected = module()
-                        )
-
-})
-
+  })
 
   # Create module name column for course items dataframe
   tower_item_new <- reactive({
@@ -107,40 +103,39 @@ overviewModule <- function(input, output, session) {
     create_module_name(item_df = tower_item())
   })
 
-   ##################################################
-   ############        Tower  Plot      #############
-   ##################################################
-
-   # Filter student demographic
-   filtered_tower_engage <- reactive({
-
-
-       tower_engage <- filter_demographics(tower_engage(),
-                                           gender = input$gender,
-                                           mode = input$mode,
-                                           activity_level = input$activity_level)
-
-
-       # Summarize how many learners engaged with each course item
-       tower_engage <- get_nactive(detail_df = tower_engage)
-
-       return(tower_engage)
-
-
-   })
-
-   # Filter chapter
-   filtered_tower_item <- reactive({
-       tower_item <- filter_chapter_overview(input_df = tower_item_new(),
-                                             module = input$module)
-
-       return(tower_item)
-
-   })
-
+  ##################################################
+  ############        Tower  Plot      #############
+  ##################################################
+  
+  # Filter student demographic
+  filtered_tower_engage <- reactive({
+    
+    
+    tower_engage <- filter_demographics(tower_engage(),
+                                        gender = input$gender,
+                                        mode = input$mode,
+                                        activity_level = input$activity_level)
+    
+    # Summarize how many learners engaged with each course item
+    tower_engage <- get_nactive(detail_df = tower_engage)
+    
+    return(tower_engage)
+    
+  })
+  
+  # Filter chapter
+  filtered_tower_item <- reactive({
+    
+    tower_item <- filter_chapter_overview(input_df = tower_item_new(),
+                                          module = input$module)
+    
+    return(tower_item)
+    
+  })
 
   # Join engagement dataframe and item dataframe together
   reactive_tower_df <- reactive({
+    
     validate(need(try(nrow(filtered_tower_engage()) > 0), "No engagement found."))
     tower_df <- join_engagement_item(filtered_engagement = filtered_tower_engage(),
                                       filtered_item = filtered_tower_item())
@@ -149,12 +144,12 @@ overviewModule <- function(input, output, session) {
   })
 
   # Make tower plot visualization
-  output$tower_plot <- plotly::renderPlotly ({
+  output$tower_plot <- plotly::renderPlotly({
+    
     make_engagement_eiffel_tower(reactive_tower_df())
 
-})
+  })
 
   output$df <- renderDataTable(reactive_tower_df())
-
 
 }
