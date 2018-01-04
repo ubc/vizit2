2d2ce13997f32bf6a8e98088114862bddbf2ec68
David Laing <davidkendalllaing@gmail.com>
2018-01-04 11:41:15 -0800

    fix code style

--- r-package/R/link_page_server.R
+++ r-package/R/link_page_server.R
@@ -5,41 +5,47 @@
 #' @examples
 #' filter_chapter(tower_item, "all")
 filter_chapter_linkpage <- function(input_df, module = "All") {
-                   
-                  if(is.null(module)){
-                    module = "All"
-                  }
   
-                  if (module == "All") {
-                    return(input_df)
-                    }
-                  else {
-                    filtered_df <- input_df %>%
-                      filter(chapter_location == module) 
-                  return(filtered_df)
-                  }  
+  if(is.null(module)){
+    module = "All"
+  }
+  
+  if (module == "All") {
+    return(input_df)
+  }
+  else {
+    filtered_df <- input_df %>%
+      filter(chapter_location == module) 
+    return(filtered_df)
+  }  
 }     
                      
 
 
-#'  Create a module name vector sorted by the course structure index. This vector is used in the module filtering select box in ui.R 
+#'  Create a module name vector sorted by the course structure index. This 
+#'   vector is used in the module filtering select box in ui.R 
 #' @param item_df 
 #' @return chap_name
-
 get_module_vector <- function(item_df) {
   
-  chap_name <- as.vector((item_df %>%
-      arrange(index) %>% 
-      filter(category == "chapter") %>% select(name))$name)
-
-return(chap_name)
-
+  chap_name <- as.vector(
+    (item_df %>%
+       arrange(index) %>% 
+       filter(category == "chapter") %>% 
+       select(name)
+    )$name
+  )
+  
+  return(chap_name)
+  
 }
 
 
-#' Count the pageview of each page.Here, we set a threshold for only counting pages have been viewed by more than certain amount of students. 
+#' Count the pageview of each page.Here, we set a threshold for only counting 
+#'   pages have been viewed by more than certain amount of students. 
 #' @param filtered_log_df A page related tracklog dataframe after filtering.
-#' @return page_student A page summary dataframe containing the pageview of each pages.
+#' @return page_student A page summary dataframe containing the pageview of each 
+#'   pages.
 #' @export
 #' @examples 
 #' get_pageview(filtered_log_df = log_dat)
@@ -60,7 +66,8 @@ get_pageview <- function(filtered_log_df){
 
 
 #' Get the name description of each page.
-#' @param page_name_df A dataframe we get after joining page dataframe and page name dataframe.
+#' @param page_name_df A dataframe we get after joining page dataframe and page 
+#'   name dataframe.
 #' @return each_page
 #' @export
 #' @examples 
@@ -83,42 +90,53 @@ get_page_name <- function(page_name_df){
 }
 
 
-
 #' Remove duplicated name for each page.
 #' @param each_page_df 
 #' @return all_pages
 #' @examples get_unique_page_name(each_page_df = each_page)
 get_unique_page_name <- function(each_page_df){
-
+  
   # Get unique page name rows
   each_page_df <- each_page_df[!duplicated(each_page_df[1]),]
   
- if(nrow(each_page_df)!=0){
-  
-          # Create a hyperlink column
-          each_page_df["url_link"] <- paste0("<a href='",each_page_df$page,"'>",each_page_df$name,"</a>") 
-  
-          # Select five columns to for the table later
-          all_pages <- each_page_df %>% 
-          select(name,url_link,index,category,num_student,chapter_location)
-          
-          # Make the page index consitent
-          all_pages$index <- seq(nrow(all_pages))
-          all_pages$index <- (max(all_pages$index)+1) - all_pages$index
-          
-         colnames(all_pages) <- c('Page_Description','Page_url','Page_Index','Page_Category','Pageview','Module')
-          
-         
- }else{
-        all_pages <- data.frame(warning = "No pages found")   
-         } 
+  if(nrow(each_page_df)!=0){
+    
+    # Create a hyperlink column
+    each_page_df["url_link"] <- paste0(
+      "<a href='",
+      each_page_df$page,
+      "'>",
+      each_page_df$name,
+      "</a>"
+    ) 
+    
+    # Select five columns to for the table later
+    all_pages <- each_page_df %>% 
+      select(name,url_link,index,category,num_student,chapter_location)
+    
+    # Make the page index consitent
+    all_pages$index <- seq(nrow(all_pages))
+    all_pages$index <- (max(all_pages$index)+1) - all_pages$index
+    
+    colnames(all_pages) <- c('Page_Description',
+                             'Page_url',
+                             'Page_Index',
+                             'Page_Category',
+                             'Pageview',
+                             'Module')
+    
+    
+  }else{
+    all_pages <- data.frame(warning = "No pages found")   
+  } 
   return(all_pages)   
-
+  
 }
 
 
 
-#' create a page summary table to show page(name,clickable hyperlink) and its page view sorted by pageview in descending order
+#' Create a page summary table to show page (name, clickable hyperlink) and its 
+#'    page view sorted by pageview in descending order
 #' @param page_summary 
 #' @return page_df
 #' @export
@@ -126,22 +144,23 @@ get_unique_page_name <- function(each_page_df){
 #' creat_page_table(page_summary = reactive_page())
 creat_page_table <- function(page_summary){
   
-  if((nrow(page_summary)!= 1) & (nrow(page_summary)!= 0)){
-      
-        page_df <- page_summary %>% 
-        select(Page_url,Pageview) %>% 
-        arrange(desc(Pageview))
-      
-      colnames(page_df) <- c("Page URL","Pageviews")
-    }else{
-      page_df <- page_summary
-    }
+  if ((nrow(page_summary)!= 1) & (nrow(page_summary)!= 0)) {
+    
+    page_df <- page_summary %>% 
+      select(Page_url,Pageview) %>% 
+      arrange(desc(Pageview))
+    
+    colnames(page_df) <- c("Page URL","Pageviews")
+    
+  } else {
+    
+    page_df <- page_summary
+  
+  }
   
   return(page_df)
   
 }
-
-
 
 
 #' Compute how many times each external link have been clicked.
@@ -154,38 +173,38 @@ get_click_per_link <- function(link_df){
   min_click_threshold <- 5 
   
   # If link data is not NONE after two two type of filtering 
-  if (nrow(link_df)!=0){
-  
-      
-      # Compute the number of click on each page
-      link_num <- link_df %>%
-        filter(!is.na(link)) %>%
-        group_by(link) %>% 
-        summarise(number_of_click = n()) %>% 
-        filter(number_of_click >= min_click_threshold) 
-      
-  
-      # Clean link to the tidy form
-      link_num$link <- substr(link_num$link,17,nchar(link_num$link)-4)
-      
-      link_num <- link_num %>% 
-       filter(link!= "http://open.edx.org/") 
-  
-  }else{
+  if (nrow(link_df)!=0) {
     
-       link_num <- data.frame("Warning" = "No link found")
+    
+    # Compute the number of click on each page
+    link_num <- link_df %>%
+      filter(!is.na(link)) %>%
+      group_by(link) %>% 
+      summarise(number_of_click = n()) %>% 
+      filter(number_of_click >= min_click_threshold) 
+    
+    
+    # Clean link to the tidy form
+    link_num$link <- substr(link_num$link,17,nchar(link_num$link)-4)
+    
+    link_num <- link_num %>% 
+      filter(link != "http://open.edx.org/") 
+    
+  } else {
+    
+    link_num <- data.frame("Warning" = "No link found")
     
   }
   
-   # Output link table
-   return(link_num)
-  
+  # Output link table
+  return(link_num)
   
 }
 
 
 
-#' Create a page summary table to show page(name,clickable hyperlink) and its page view sorted by pageview in descending order
+#' Create a page summary table to show page(name,clickable hyperlink) and its 
+#'   page view sorted by pageview in descending order
 #' @param page_summary 
 #' @return page_df
 #' @export
@@ -194,19 +213,26 @@ get_click_per_link <- function(link_df){
 creat_link_table <- function(link_summary){
   
   # If link summary table is not 1-row warning and empty table   
-     if ((nrow(link_summary)!= 1) & (nrow(link_summary)!= 0)){
+  if ((nrow(link_summary)!= 1) & (nrow(link_summary)!= 0)){
     
-      link_df <- link_summary %>% 
+    link_df <- link_summary %>% 
       arrange(desc(number_of_click)) 
-      
-      link_df$link <-  paste0("<a href='",link_df$link,"'>",substr(link_df$link,1,60),"</a>")
-      colnames(link_df) <- c('Link','Number of Clicks') 
-   
-  # If link summary table is 1-row warning or empty table  
-    }else{
-    link_df <- data.frame( Warning = "No link found")
-    }
     
+    link_df$link <-  paste0(
+      "<a href='",
+      link_df$link,
+      "'>",
+      substr(link_df$link,1,60),
+      "</a>"
+    )
+    
+    colnames(link_df) <- c('Link','Number of Clicks') 
+    
+  } else {
+    # If link summary table is 1-row warning or empty table
+    link_df <- data.frame( Warning = "No link found")
+  }
+  
   return(link_df)
   
 }
