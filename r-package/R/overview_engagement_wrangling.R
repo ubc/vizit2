a247d8e83e56ef88dfe1a6c618ac07b62d8f7b87
David Laing <davidkendalllaing@gmail.com>
2018-01-04 13:31:21 -0800

    clean up

--- r-package/R/overview_engagement_wrangling.R
+++ r-package/R/overview_engagement_wrangling.R
@@ -1,11 +1,14 @@
 #' Import untidy student engagement csv file as an R dataframe object
-#' @param input_course Name of course directory (ex. psyc1, spd1, marketing, etc)
-#' @return tower_engage: Dataframe containing student engagement information with videos and problems
+#' @param input_course Name of course directory (eg. psyc1, spd1, etc)
+#' @return tower_engage: Dataframe containing student engagement information 
+#'   with videos and problems
 #' @examples 
 #' read_dirt_engagement_data(input_course = 'psyc1')
 #' @export
 read_dirt_engagement_data <- function(input_course){
-  input_csv_path <- paste0("../inst/data/", input_course, "/tower_engage_dirt.csv")
+  input_csv_path <- paste0("../inst/data/", 
+                           input_course, 
+                           "/tower_engage_dirt.csv")
   engage_dirt <- read_csv(input_csv_path)
   return(engage_dirt)
 }
@@ -20,10 +23,14 @@ read_dirt_engagement_data <- function(input_course){
 clean_engagement_data <- function(dirt_engagement_data){
   
   # Remove "i4x://" in column A_module_id
-  dirt_engagement_data$module_id <- gsub("i4x://", "", dirt_engagement_data$module_id)
+  dirt_engagement_data$module_id <- gsub("i4x://", 
+                                         "", 
+                                         dirt_engagement_data$module_id)
 
   # Rename column in engagement table
-  colnames(dirt_engagement_data)[colnames(dirt_engagement_data) == 'B_mode'] <- 'mode'
+  colnames(dirt_engagement_data)[
+    colnames(dirt_engagement_data) == 'B_mode'
+  ] <- 'mode'
   
   # Transform activity levels.
   dirt_engagement_data <- dirt_engagement_data %>%
@@ -40,14 +47,16 @@ clean_engagement_data <- function(dirt_engagement_data){
 }
 
 
-
 #' Write cleaned data as a csv into the specified course directory
-#' @param input_course Name of course directory (ex. psyc1, spd1, marketing, etc)
+#' @param input_course Name of course directory (ex. psyc1, spd1, etc)
 #' @param cleaned_data Dataframe containing cleaned data. 
 #' @examples 
-#' write_tidy_engagement_data(input_course = 'psyc1', cleaned_data = clean_engagement_data)
+#' write_tidy_engagement_data(
+#'   input_course = 'psyc1', 
+#'   cleaned_data = clean_engagement_data
+#' )
 #' @export
-write_tidy_engagement_data <- function(input_course, cleaned_data){
+write_tidy_engagement_data <- function(input_course, cleaned_data) {
   
   # Make IO path
   output_csv_path <- paste0("../inst/data/", input_course, "/tower_engage.csv")
@@ -57,9 +66,9 @@ write_tidy_engagement_data <- function(input_course, cleaned_data){
 }
 
 
-
 #' This function automatically reads a file named "tower_engage_dirt.csv"
-#' from the specified course directory and output a clean files named "tower_engage.csv" in the same directory
+#'   from the specified course directory and output a clean files named 
+#'   "tower_engage.csv" in the same directory
 #' @param input_course String of course directory name 
 #' @examples
 #' wrangle_video(input_course = "psyc1")
@@ -73,7 +82,7 @@ wrangle_overview_engagement <- function(input_course){
   tidy_engage <- clean_engagement_data(dirt_engage)
 
   # Export data
-  write_tidy_engagement_data(input_course = input_course, cleaned_data= tidy_engage)
-
+  write_tidy_engagement_data(input_course = input_course, 
+                             cleaned_data= tidy_engage)
 
 }
